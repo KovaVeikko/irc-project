3c958f489ebcfc1fc65ec6932905b6d207492232
Veikko Kovanen <veikko.kovanen@gmail.com>
2017-04-02 13:45:53 +0300

    added commands

--- server/headers/commands.h
+++ server/headers/commands.h
@@ -1,6 +1,11 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
-extern char *NICK;
+extern char *INFO; // get information about the server
+extern char *NICK; // change nick
+extern char *LIST; // list all channels
+extern char *JOIN; // join channel
+extern char *PART; // leave channel
+
 
 #endif
--- server/modules/commands.c
+++ server/modules/commands.c
@@ -1,3 +1,7 @@
 #include "../headers/commands.h"
 
+char *INFO = "/INFO";
 char *NICK = "/NICK";
+char *LIST = "/LIST";
+char *JOIN = "/JOIN";
+char *PART = "/PART";
