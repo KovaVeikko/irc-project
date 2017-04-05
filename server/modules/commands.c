13d3e50c451c521932d48245888228a3521c8052
Veikko Kovanen <veikko.kovanen@gmail.com>
2017-04-05 13:12:31 +0300

    fix command parsing

--- server/modules/commands.c
+++ server/modules/commands.c
@@ -1,7 +1,7 @@
 #include "../headers/commands.h"
 
-char *INFO = "/INFO";
-char *NICK = "/NICK";
-char *LIST = "/LIST";
-char *JOIN = "/JOIN";
-char *PART = "/PART";
+char *INFO = "INFO";
+char *NICK = "NICK";
+char *LIST = "LIST";
+char *JOIN = "JOIN";
+char *PART = "PART";
--- server/modules/connect.c
+++ server/modules/connect.c
@@ -7,18 +7,25 @@ void client_thread(Client *client) {
 
   // receive message from client
   char received_message[MAXLINE + 1];
+  char *msg = malloc(MAXLINE + 1);
+  char *cmd;
   ssize_t n = 1;
 
   n = read(client -> socket, received_message, MAXLINE);
 
   while (n > 0) {
     received_message[n] = 0;
-    // printf("%s: %s", client -> name, received_message);
-    if(strstr(received_message, NICK) != NULL) {
-      //TODO: command NICK without parameters causes segmentation fault
-      handle_nick(client, clients_stack, strtok(received_message + strlen(NICK) + 1, "\n"));
-    } else {
-      send_message(client, clients_stack, received_message);
+    msg = strcpy(msg, received_message);
+    cmd = strtok(msg, " \t\r\n/");
+    while(cmd != NULL) {
+      printf("%s sent: \"%s\"\n", client -> name, cmd);
+      if(strcmp(cmd, NICK) == 0) {
+        handle_nick(client, clients_stack, strtok(NULL, " \t\r\n/"));
+      } 
+      else {
+        send_message(client, clients_stack, received_message);
+      }
+      cmd = strtok(NULL, " \t\r\n/");
     }
     n = read(client -> socket, received_message, MAXLINE);
   }
